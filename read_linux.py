fbcd036007b2659f1a5fa18bd27684b0360f4c4d
jayan04mcc <104178976+jayan04mcc@users.noreply.github.com>
2023-12-26 15:09:05 -0500

    archivo read para windows y linux

--- read_linux.py
+++ read_linux.py
@@ -1,11 +1,21 @@
+import subprocess
 import tkinter as tk
-# Abre el archivo en modo lectura
-with open('tu_archivo.txt', 'r') as archivo_entrada:
+
+# Nombre del archivo de salida
+nombre_archivo = 'salida.txt'
+
+# Ejecutar un comando y redirigir la salida a un archivo de texto
+with open(nombre_archivo, 'w') as archivo:
+    subprocess.run('ps -eo pid,comm,%cpu,%mem --sort=-%cpu | head', shell=True, stdout=archivo, text=True)
+
+print(f"La salida del comando se ha guardado en {nombre_archivo}")
+
+with open(nombre_archivo, 'r') as archivo_entrada:
     # Lee todas las líneas del archivo
     lineas = archivo_entrada.readlines()
 
 # Elimina los espacios en blanco al inicio de cada línea y sobrescribe el archivo original
-with open('tu_archivo.txt', 'w') as archivo_salida:
+with open(nombre_archivo, 'w') as archivo_salida:
     for linea in lineas:
         linea_sin_espacios = linea.lstrip()  # Elimina espacios en blanco al inicio
         archivo_salida.write(linea_sin_espacios)
@@ -13,11 +23,11 @@ with open('tu_archivo.txt', 'w') as archivo_salida:
 tercera_columna = []
 lista_total=[]
 # Lee el contenido del archivo modificado
-with open('tu_archivo.txt', 'r') as archivo_modificado:
+with open(nombre_archivo, 'r') as archivo_modificado:
     for linea in archivo_modificado:
         if not linea.strip():
             continue  # Ignorar líneas vacías
-        
+
         palabras = linea.split()  # Ajustar según el delimitador correcto
         if len(palabras) >= 3:
             tercera_columna.append(palabras[2].strip())

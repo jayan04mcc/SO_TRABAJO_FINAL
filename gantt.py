83a4bc77e127d2bca7178583648aa579c81d8fd3
YezzuzBruno <jchancayauris@unsa.edu.pe>
2024-01-04 13:17:10 -0500

    Se logra mostrar el diagrama de Gantt de ejemplo

--- gantt.py
+++ gantt.py
@@ -29,25 +29,30 @@ gantt.grid(True, axis='y', which='minor')
 
 # Etiquetas de máquinas:
 gantt.set_yticks(np.arange(hbar/2, hbar*nmaq - hbar/2 + hbar,
-                           hbar))
+                            hbar))
 gantt.set_yticklabels(maquinas)
 
 # Función para armar tareas:
 def agregar_tarea(t0, d, maq, nombre, color):
-    # Índice de la máquina:
+# Índice de la máquina:
     imaq = maquinas.index(maq)
     # Posición de la barra:
     gantt.broken_barh([(t0, d)], (hbar*imaq, hbar),
-                      facecolors=(color))
+                        facecolors=(color))
     # Posición del texto:
     gantt.text(x=(t0 + d/2), y=(hbar*imaq + hbar/2),
-                  s=f"{nombre} ({d})", va='center', color='white')
+                    s=f"{nombre} ({d})", va='center', color='white')
 
 # Agregamos dos tareas de ejemplo:
 agregar_tarea(35, 15, "P1", "T1", "r")
 agregar_tarea(10, 20, "P3", "T1", "g")
 
-# Ploteamos
-#plt.show()
 
-fig.savefig("gantt_chart.png", bbox_inches='tight', dpi=150)
+# Guarda la imagen en la ruta especificada
+ruta_imagen = "static/img/gantt.svg"
+fig.savefig(ruta_imagen)
+
+# Cierra la figura para liberar memoria
+plt.close(fig)
+
+#plt.show()
--- index.py
+++ index.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
+import subprocess
+
 from x import lista_procesos
 from fcfs import procesos, respuestas
-
+#from gantt import mostrar
 
 app = Flask(__name__)
 
@@ -10,10 +13,17 @@ app = Flask(__name__)
 def principal():
     return render_template("index.html", procesos=lista_procesos, resultados=procesos, tiempos=respuestas) #buscara la vista y la mostrara
 
-#definiendo otra ruta
-@app.route("/contacto")
-def contacto():
-    return "Esta es la pagina de contacto"
+@app.route('/gantt_diagram', methods=['GET', 'POST'])
+def gantt_diagram():
+    # Aquí puedes ejecutar tu función o archivo de Python
+    # Por ejemplo, para ejecutar un archivo:
+    os.system('python gantt.py')
+    # En este ejemplo, simplemente servimos un archivo de imagen estático como respuesta
+    return render_template("gantt.html")
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
